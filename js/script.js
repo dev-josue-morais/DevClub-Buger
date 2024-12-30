e95fe8d1f97cea1fa6127acee6facf50fdd2c5b0
josue28jrd <josue28jrd@hotmail.com>
2024-12-30 08:26:35 -0300

    save project

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-
-test = 500
-
-test > 300 ? console.log("maior 500") : console.log("menor 500")
-
+// IMPORTS
+const buttonAll = document.querySelector("#all");
+const buttonMap = document.getElementById("map");
+const buttonReduce = document.getElementById("reduce");
+const buttonFilter = document.getElementById("filter");
+const unordenedList = document.querySelector("ul");
+const list = document.querySelector("li");
+const button = document.querySelector("button")
+// menuOptions.forEach(element => {
+    
+// });
+console.log(buttonAll)
+console.log(buttonMap)
+console.log(buttonReduce)
+console.log(buttonFilter)
+console.log(unordenedList)
+console.log(list)
