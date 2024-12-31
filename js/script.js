6d12218ba2aa90157cbaf756e8ac651cf494d2d4
josue28jrd <josue28jrd@hotmail.com>
2024-12-31 08:32:31 -0300

    fisnish

--- js/script.js
+++ js/script.js
@@ -4,86 +4,75 @@ const buttonMap = document.getElementById("map");
 const buttonReduce = document.getElementById("reduce");
 const buttonFilter = document.getElementById("filter");
 const unordenedList = document.querySelector("ul");
-const list = document.querySelector("li");
-const button = document.querySelector("button")
+const button = document.querySelector("button");
 
-const menuOptionsa = [
-
-    { name: 'X-Salada', price: 30, vegan: false, src: './assets/images/xsalada.jpeg' },
-
-    { name: 'X-Bacon', price: 34, vegan: false, src: './assets/images/xbacon.png' },
-
-    { name: 'X-Bacon Egg', price: 39, vegan: false, src: './assets/images/bacon-egg.png' },
-
-    { name: 'Monstruoso', price: 50, vegan: false, src: './assets/images/monstruoso.png' },
-
-    { name: 'Big Vegano', price: 55, vegan: true, src: './assets/images/xvegan.png' },
-
-    { name: 'X-Vegan', price: 45, vegan: true, src: './assets/images/monstruoso-vegan.png' },
-
-]
-
-function clearList(list) {
-    while (list.firstChild) {
-        list.removeChild(list.firstChild);
+function clearList(listElement) {
+    while (listElement.firstChild) {
+        listElement.removeChild(listElement.firstChild);
     }
 }
 
-function createListItem({ name, price, vegan, src }) {
-    const li = document.createElement('li');
+function createList({ name, vegan, price, src }) {
+    const li = document.createElement("li");
     li.className = vegan ? "isVegan" : "notVegan";
-    const img = document.createElement('img');
+    const img = document.createElement("img");
     img.src = src;
     img.alt = name;
-    const p1 = document.createElement('p');
+    const p1 = document.createElement("p")
     p1.textContent = name;
-    const p2 = document.createElement('p');
+    const p2 = document.createElement("p");
     p2.textContent = `R$ ${price.toFixed(2)}`;
     p2.className = "item-price";
     li.appendChild(img);
     li.appendChild(p1);
     li.appendChild(p2);
-    return li;
+    return li
 }
-
-const discount = menuOptions.map(item => ({
-    ...item,
-    price: (item.price * 0.9),
-}));
 
 buttonAll.addEventListener("click", () => {
     clearList(unordenedList);
-    menuOptions.forEach(item => {
-        const listItem = createListItem(item);
-        unordenedList.appendChild(listItem);
+    menuOptions.forEach(element => {
+        const list = createList(element)
+        unordenedList.appendChild(list)
     });
 });
 
+
 buttonMap.addEventListener("click", () => {
     clearList(unordenedList);
-    discount.forEach(item => {
-        const listItem = createListItem(item);
-        unordenedList.appendChild(listItem);
+    menuOptions.map((item,) => ({ ...item, price: item.price * 0.9}))
+    .forEach(element => {
+        const list = createList(element)
+        unordenedList.appendChild(list)
     });
 });
 
 buttonReduce.addEventListener("click", () => {
     clearList(unordenedList);
-
-    const totalPrice = menuOptions.reduce((acc, { price }) => acc + price, 0);
-
-    const li = document.createElement('li');
-    const img = document.createElement('img');
+    const sum = menuOptions.reduce((acc, {price}) => acc + price,0);
+    const li = document.createElement("li");
+    const img = document.createElement("img");
     img.src = './assets/images/xsalada.jpeg';
-    img.alt = "burger";
-    const p1 = document.createElement('p');
+    img.alt = "Burger";
+    const p1 = document.createElement("p")
     p1.textContent = "Valor Total";
-    const p2 = document.createElement('p');
-    p2.textContent = `R$ ${totalPrice.toFixed(2)}`;
+    const p2 = document.createElement("p");
+    p2.textContent = `R$ ${sum.toFixed(2)}`;
     p2.className = "item-price";
     li.appendChild(img);
     li.appendChild(p1);
     li.appendChild(p2);
-
     unordenedList.appendChild(li);
 });
+
+buttonFilter.addEventListener("click", () => {
+    clearList(unordenedList);
+    menuOptions.filter(item => item.vegan && true )
+    .forEach(element => {
+        const list = createList(element)
+        unordenedList.appendChild(list)
+    });    
+});
+
+
+
